0f6a38fa415af05e4add6d74b589bc24d84aa08c
Sasaki <mailes2@keeling-c01.earth.uiuc.edu>
2025-02-20 14:55:08 -0600

    Updated geoparquet code

--- calculations/.ipynb_checkpoints/LOCA2_predagster-checkpoint.py
+++ calculations/.ipynb_checkpoints/LOCA2_predagster-checkpoint.py
@@ -10,6 +10,11 @@ import pandas as pd
 
 class OpenLocaCat:
     def __init__(self):
+        """
+        Initialization
+
+        Opens LOCA2 Catalog
+        """
         load_dotenv()
 
         url = "s3://ees240146/loca2_zarr_monthly_esm_catalog.json"
@@ -24,6 +29,24 @@ class OpenLocaCat:
         print(catalog)
     
     def load(self, query):
+        """
+        Loads a LOCA2 dataset according to a user's query
+
+        Input:
+            - query (Dictionary) - A query organized as so:
+                       ex: query = dict(variable=['tasmax','pr'],
+                                        model=['ACCESS-CM2'],
+                                        scheme=["ssp370"],
+                                        experiment_id=['r1i1p1f1'])
+                    The user may leave out as many keys or values as they want. If a key is left out, 
+                    it is assumed that the user will want all values of that key and return them all. 
+                    (ex: No variable is given, so the model returns every variable available)
+
+        Output:
+            - dataset_full (Dataset) - Dataset with the dimensions model, scheme, experiment_id, lat, lon, and time.
+                    Variables can be 'tasmax', 'pr', or 'tasmin'. All empty values are replaced with NaNs.
+
+        """
         catalog_subset = self.catalog.search(**query)
         
         dsets = catalog_subset.to_dataset_dict(
@@ -87,7 +110,7 @@ class OpenLocaCat:
                         else: # If no datasets for this set of parameters
                             # Create an empty dataset with the correct dimensions and labeled accordingly
                             empty_dataset = xr.DataArray(
-                                                    np.empty((sum_time,474,944)),
+                                                    np.full((sum_time,474,944), np.nan),
                                                     dims=['time','lat','lon']).to_dataset(name=variable + '_tavg')
                             empty_dataset.coords['member_id'] = member_id
                             empty_dataset.coords['scheme'] = scheme
@@ -106,38 +129,59 @@ class OpenLocaCat:
         
         return dataset_full
         
-    def stats(self, dataset):
+    def stats(self, dataset, coords):
         """
         Calculates statistics across the models for all variables
     
         Input:
-            - dataset (Dataset or Dataarray) - Needs a dimension called "model"
+            - dataset (Dataset or Dataarray) - An xarray dataset to have means done across each variable
+            - coords (List of strings) - Takes mean across each dim. given in order of the list
         
         Output:
             - data_stats (Dataset) - Contains calculations of the mean, standard deviation, and variance
                 of the dataset. All statistics stored in a coordinate called "stats"
             
         """
-    
-        mean = dataset.mean('model')
-        mean['stats'] = 'mean'
-    
-        stdev = dataset.std('model')
-        stdev['stats'] = 'stdev'
-    
-        variance = dataset.var('model')
-        variance['stats'] = 'variance'
-    
-        data_stats = xr.concat([mean, stdev, variance], 'stats')
+
+        # Initializing each dataset for iteration
+        dataset_mean = dataset
+        dataset_stdev = dataset
+        dataset_var = dataset
+
+
+        # Iterating over each coordinate
+        for coord in coords:
+            dataset_mean = dataset_mean.mean(coord)
+            dataset_stdev = dataset_stdev.std(coord)
+            dataset_var = dataset_var.var(coord)
+            
+        dataset_mean['stats'] = 'mean'
+        dataset_stdev['stats'] = 'stdev'
+        dataset_var['stats'] = 'variance'
+
+        # Combining datasets along a coordinate called stats
+        data_stats = xr.concat([dataset_mean, dataset_stdev, dataset_var], 'stats')
         return data_stats
     
     def illinois(self, dataset):
+        """
+        Returns the dataset to the area surrounding Illinois
+
+        """
         dataset_ill = dataset.sel(lat=slice(36,43.5)).sel(lon=slice(267.2,274))
         
         return dataset_ill
 
     def parquet(self, dataset, filename):
-        
+        """
+        Sends dataset to a GeoParquet file
+
+        Input:
+            - dataset (Dataset) - An xarray dataset with a lat and a lon coordinate
+            - filename (string) - File to save GeoParquet to
+
+        No output, but saves a file at filename
+        """
         dataframe = dataset.to_dataframe()
         dataframe_reset = dataframe.reset_index()
         gdf = geopandas.GeoDataFrame(
@@ -146,6 +190,9 @@ class OpenLocaCat:
                 crs="NAD83",
                 )
 
+        del gdf['lat']
+        del gdf['lon']
+        
         gdf.to_parquet(filename)
 
 
